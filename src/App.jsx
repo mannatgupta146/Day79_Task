db0e183d622b5508afbe55cff9de6f8759a2c277
mannatgupta146 <mannatgupta146@gmail.com>
2026-01-04 13:55:48 +0530

    learnng routes and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,15 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
 
 const App = () => {
-
-  const [counter, setCounter] = useState(0)
-
-  const [name, setName] = useState('')
-  useEffect(()=>{
-    console.log("hi")
-  })
-
   return (
     <div>
-      <input 
-      type="text" 
-      placeholder='enter name'
-      value={name}
-      onChange={(e)=>{
-        setName(e.target.value)
-      }}
-      />
-
-      <h1>{counter}</h1>
-      <button onClick={()=>{
-        setCounter(counter+1)
-      }}>
-        Increment Counter
-      </button>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About/>} />
+      </Routes>
     </div>
   )
 }
